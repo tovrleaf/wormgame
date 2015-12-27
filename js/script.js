8de96d8253bc00b27a6eea0fe30d924dab328a96
Sari Myllynen <sari.myllynen@windowslive.com>
2015-12-27 23:14:45 +0200

    First version

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var fromLeft = 0;
+var fromTop = 0;
+
+function moveLeft() {
+    fromLeft = fromLeft - 10;
+    if (fromLeft < 0) {
+        fromLeft = 0;
+        return;
+    }
+    $("#worm").css({
+        left: fromLeft + 'px'
+    });
+}
+
+function moveUp() {
+    fromTop = fromTop - 10;
+    if (fromTop < 0) {
+        fromTop = 0;
+        return;
+    }
+    $("#worm").css({
+        top: fromTop + 'px'
+    });
+}
+
+function moveDown() {
+    fromTop = fromTop + 10;
+    if (fromTop >= 100) {
+        fromTop = fromTop - 10;
+        return;
+    }
+    $("#worm").css({
+        top: fromTop + 'px'
+    });
+}
+
+function moveRight() {
+    fromLeft = fromLeft + 10;
+    if (fromLeft >= 100) {
+        fromLeft = fromLeft - 10;
+        return;
+    }
+    $("#worm").css({
+        left: fromLeft + 'px'
+    });
+}
