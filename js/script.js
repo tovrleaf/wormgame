b5b528802987389190740b93e2273a4069babc86
Niko Kivelä <niko@tovrleaf.com>
2016-01-02 13:58:35 +0200

    Worm grows using intervals

--- js/script.js
+++ js/script.js
@@ -1,79 +1,104 @@
-var fromLeft, fromTop, gameareaOffset;
+var fromLeft, fromTop, gameareaOffset, direction;
+
+setInterval(function() {;
+    switch (direction) {
+    case "right":
+        moveRight();
+      break;
+    case "down":
+        moveDown();
+      break;
+    case "left":
+        moveLeft();
+      break;
+    case "up":
+        moveUp();
+      break;
+    default: // default omitted
+        break;
+    }
+
+  if (direction) {
+      growWorm(fromLeft, fromTop, direction);
+    }
+}, 1000);
 
 $(document).ready(function() {
     resetGame();
 });
 
-function moveLeft() {
-    fromLeft = fromLeft - 10;
-    if (fromLeft < gameareaOffset.left) {
-        fromLeft = gameareaOffset.left;
-        return;
-    }
-    growWorm(fromLeft, fromTop, "left");
-}
+function setDirectionRight() { direction = "right"; }
+function setDirectionDown() { direction = "down"; }
+function setDirectionLeft() { direction = "left"; }
+function setDirectionUp() { direction = "up"; }
 
-function moveUp() {
-    fromTop = fromTop - 10;
-    if (fromTop < gameareaOffset.top) {
-        fromTop = gameareaOffset.top;
-        return;
-    }
-    growWorm(fromLeft, fromTop, "up");
+function moveRight() {
+  fromLeft = fromLeft + 10;
+  if (fromLeft >= 100 + gameareaOffset.left) {
+    fromLeft = fromLeft - 10;
+    return;
+  }
 }
 
 function moveDown() {
-    fromTop = fromTop + 10;
-    if (fromTop >= 100 + gameareaOffset.top) {
-        fromTop = fromTop - 10;
-        return;
-    }
-    growWorm(fromLeft, fromTop, "down");
+  fromTop = fromTop + 10;
+  if (fromTop >= 100 + gameareaOffset.top) {
+    fromTop = fromTop - 10;
+    return;
+  }
 }
 
-function moveRight() {
-    fromLeft = fromLeft + 10;
-    if (fromLeft >= 100 + gameareaOffset.left) {
-        fromLeft = fromLeft - 10;
-        return;
-    }
-    growWorm(fromLeft, fromTop, "right");
+function moveLeft() {
+  fromLeft = fromLeft - 10;
+  if (fromLeft < gameareaOffset.left) {
+    fromLeft = gameareaOffset.left;
+    return;
+  }
+}
+
+function moveUp() {
+  fromTop = fromTop - 10;
+  if (fromTop < gameareaOffset.top) {
+    fromTop = gameareaOffset.top;
+    return;
+  }
 }
 
 function growWorm(x, y, direction) {
-    var last = $("#gamearea .worm:last");
-    last.removeClass("head");
-    if (direction == "left") {
-        last.addClass("right");
-    }
-    if (direction == "down") {
-        last.addClass("up");
-    }
-    if (direction == "up") {
-        last.addClass("down");
-    }
+  var last = $("#gamearea .worm:last");
+  last.removeClass("head");
+  if (direction == "left") {
+    last.addClass("right");
+  }
+  if (direction == "down") {
+    last.addClass("up");
+  }
+  if (direction == "up") {
+    last.addClass("down");
+  }
     if (direction == "right") {
-        last.addClass("left");
-    }
-    if (last.hasClass("left") && last.hasClass("right")) {
-        last.addClass("fullWidth");
-    }
-    if (last.hasClass("up") && last.hasClass("down")) {
-        last.addClass("fullHeight");
-    }
+    last.addClass("left");
+  }
+  if (last.hasClass("left") && last.hasClass("right")) {
+    last.addClass("fullWidth");
+  }
+  if (last.hasClass("up") && last.hasClass("down")) {
+      last.addClass("fullHeight");
+  }
     $(createWormElement()).addClass(direction).appendTo("#gamearea").offset({
-        left: x,
-        top: y
-    });
+    left: x,
+    top: y
+  });
 }
 
 function createWormElement() {
-    return '<div class="worm head"></div>';
+  return '<div class="worm head"></div>';
 }
 
 function resetGame() {
-    $("#gamearea").html(createWormElement());
-    gameareaOffset = $("#gamearea .worm").offset();
-    fromLeft = gameareaOffset.left;
-    fromTop = gameareaOffset.top;
+  $("#gamearea").html(createWormElement());
+  gameareaOffset = $("#gamearea .worm").offset();
+  fromLeft = gameareaOffset.left;
+  fromTop = gameareaOffset.top;
+  direction = null;
 }
